c312bcec9ee715b4e67a612d3c22cbb611350be0
Fitrah-16520207 <fitnug30@gmail.com>
2021-10-26 23:09:21 +0700

    Update konfigurasi.h

--- src/konfigurasi.h
+++ src/konfigurasi.h
@@ -11,43 +11,60 @@
 #include "matrix.h"
 #include "wordmachine.h"
 #include "charmachine.h"
+#include "pesanan.h"
 #include "point.h"
-#include "ListDin.h"
 
 typedef struct { 
 	POINT loc[CAPACITY]; /* Koordinat lokasi*/
-	int m.nEff; /* >=0 banyaknya elemen efektif*/
-	int hub[CAPACITY][CAPACITY]; /* list yang berhubungan dengan lokasi */
+	int nEff; /* >=0 banyaknya elemen efektif*/
+	int hub[CAPACITY][CAPACITY]; /*  matrix adjency yang berhubungan dengan lokasi */
+	char peta[CAPACITY][CAPACITY];/* matrix untuk menampilkan koordinat lokasi*/
+	Pesanan pesan[CAPACITY];
 } MAP;
 /* *** Notasi Akses: Selektor MAP *** */
-#define NAMA(M) (M).loc.label
-#define ABSIS(M) (M).loc.X
-#define ORDINAT(M) (M).loc.Y
-#define ELMT(M, i, j) (M).hub[(i)][(j)]
+#define NAMA(M,i) (M).loc[i].label
+#define ABSIS(M,i) (M).loc[i].X
+#define ORDINAT(M,i) (M).loc[i].Y
+#define ELMT_M(M, i, j) (M).hub[(i)][(j)]
+#define PETA(M, i, j) (M).peta[(i)][(j)]
+#define nEff(M) (M).nEff
+#define ORDER(M,i) (M).pesan[(i)]
+#define O_MASUK(M,i) (M).pesan[(i)].tMasuk
+#define O_PICK_P(M,i) (M).pesan[(i)].pickUpPoint
+#define O_DROP_P(M,i) (M).pesan[(i)].dropOffPoint
+#define O_TYPE(M,i) (M).pesan[(i)].type
+#define O_T_PERISH(M,i) (M).pesan[(i)].tPerish
 /* Jika m adalah : MAP, cara deklarasi dan akses: */
 /* Deklarasi : m : MAP */
 /* Maka cara akses:
-   m.nama  untuk mengetahui char atau nama karakter dari lokasi
-   m.idx    untuk mengakses indeks lokasi
-   m.loc untuk mengakses koordinat lokasi dengan POINT
-   m.hub untuk mengakses list yang berhubungan dengan lokasi */
+   m.loc[i].label  untuk mengetahui char atau nama karakter dari lokasi
+   m.loc[i].X  untuk mengakses absis lokasi dengan POINT
+   m.loc[i].Y untuk mengakses ordinat lokasi dengan POINT
+   m.hub untuk mengakses matrix adjency yang berhubungan dengan lokasi 
+   m.nEff untuk mengaksis ukuran MAP
+   m.peta untuk mengakses gambaran peta*/
+   
 /* Definisi :
   Map kosong: m.nEff = 0
   Definisi elemen pertama : m.loc[i] dengan i=Point
   Definisi elemen terakhir yang terdefinisi: m.loc[i] dengan i=l.nEff */
 
-
+int ConvertInt();
+	/*Mengkonvert input char dari mesin kata menjadi int*/
 void CreateMap (MAP *m);
 	/*  Membentuk map menjadi map kosong*/
+void CreatePeta (MAP *m,int i,int j);
+	/* Membentuk peta menjad peta kosong dengan ukuran i dan j*/
+
 void addMap(MAP *m,int X, int Y);
 	/* Menambah koordinat pada matrix*/
-int getElmt(Matrix m, int i,int j);
-	/* Mengirimkan elemen m(i,j) */
 
 void BacaKoordinat(MAP *m);
 	/*Membaca masukan koordinat*/
 void BacaAdjMatrix(MAP *m);
 	/*Membaca adjency matrix*/
+int LengthMAP(MAP m);
+	/*Membaca panjang map*/
 
 void BacaPesanan();
 	/*Membaca pesanan*/
