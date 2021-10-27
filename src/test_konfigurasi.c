5bd16df8efcf2199d31d0c2eff0c1dc4f0cae85f
Fitrah-16520207 <fitnug30@gmail.com>
2021-10-27 21:22:10 +0700

    Create test_konfigurasi.c

---
+++ src/test_konfigurasi.c
@@ -0,0 +1,13 @@
+#include "boolean.h"
+#include "matrix.h"
+#include "wordmachine.h"
+#include "charmachine.h"
+#include "konfigurasi.h"
+#include "point.h"
+#include "pesanan.h"
+#include <stdio.h>
+
+
+int main(){
+	printf("hello world");
+}
