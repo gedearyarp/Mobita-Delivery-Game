3c9c5d396536336af9a1c6d4779f84d9d0feeb0d
Fitrah-16520207 <fitnug30@gmail.com>
2021-10-28 10:55:20 +0700

    Create test_peta.c

---
+++ src/test_peta.c
@@ -0,0 +1,23 @@
+#include "boolean.h"
+#include "matrix.h"
+#include "wordmachine.h"
+#include "charmachine.h"
+#include "konfigurasi.h"
+#include "point.h"
+#include "pesanan.h"
+#include "peta.h"
+#include <stdio.h>
+
+
+int main(){
+	MAP M;
+	POINT p;
+	CreateMap (&M);
+	BacaKoordinat(&M);
+	BacaAdjMatrix(&M);
+	BacaPesanan(&M);
+	p = MakePOINT(5,4,'I');
+	int waktu = 5;
+	char item = 'A';
+	showPeta(M,p,waktu,item);
+}
