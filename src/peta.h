5d442490a6b734e87c0a5715c8ec531b9f282fe0
Fitrah-16520207 <fitnug30@gmail.com>
2021-10-28 10:55:17 +0700

    Create peta.h

---
+++ src/peta.h
@@ -0,0 +1,24 @@
+/* File: peta.h */
+/* *** Definisi ABSTRACT DATA TYPE POINT *** */
+
+#ifndef PETA_H
+#define PETA_H
+
+#define CAPACITY 50
+#define VAL_UNDEF -999
+
+#include "boolean.h"
+#include "matrix.h"
+#include "wordmachine.h"
+#include "charmachine.h"
+#include "pesanan.h"
+#include "point.h"
+#include "konfigurasi.h"
+#include "pcolor.h"
+
+
+void showPeta(MAP m,POINT p,int waktu);
+	//fungsi untuk menampilkan peta
+#endif
+
+
