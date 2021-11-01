244742def260e2c4283c8cd88fc55ac38cd2a5fe
GedeAryaRP <gdryrp@gmail.com>
2021-11-02 02:55:51 +0700

    Add buy and inventory

---
+++ src/inventory/inventory.h
@@ -0,0 +1,23 @@
+#ifndef INVENTORY_H
+#define INVENTORY_H
+
+#include "../boolean.h"
+
+typedef struct {
+    int gadget[6];
+    int total;
+} Inventory;
+
+#define INV(I, n) (I).gadget[n]
+#define TOT_INV(I) (I).total
+
+Inventory createInventory();
+
+void printInvetory(Inventory I);
+
+void addGadget(Inventory *I, int idxGadget);
+
+void removeGadget(Inventory *I);
+
+
+#endif
