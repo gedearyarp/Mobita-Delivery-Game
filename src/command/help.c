171c8ba9c02a385a2bdcace37995a597fd12e65e
addinnabilal <16520342@std.stei.itb.ac.id>
2021-11-07 06:04:23 +0700

    edit help

--- src/command/help.c
+++ src/command/help.c
@@ -1,13 +1,13 @@
 void helpCommand() {
-    printf("NEW GAME -> Memulai permainan\n
-    EXIT -> Menutup program\n
-    MOVE -> Berpindah sesuai posisi yang dapat dicapai\n
-    PICK_UP -> Mengambil item jika ada pesanan yang harus diambil pada lokasi\n
-    DROP_OFF -> Mengantarkan item ke lokasi jika item di tumpukan teratas sesuai dengan pesanan\n
-    MAP -> Memunculkan peta\n
-    TO_DO -> Menampilkan pesanan yang masuk ke To Do List\n
-    IN_PROGRESS -> Menampilkan pesanan yang sedang dikerjakan\n
-    BUY -> Menampilkan gadget yang dapat dibeli lalu membelinya\n
-    INVENTORY -> Menampilkan list isi inventory\n
-    HELP -> Menampilkan seluruh command yang dapat dimasukkan beserta deskripsinya\n");
+    printf("NEW GAME -> Memulai permainan\n");
+    printf("EXIT -> Menutup program\n");
+    printf("MOVE -> Berpindah sesuai posisi yang dapat dicapai\n");
+    printf("PICK_UP -> Mengambil item jika ada pesanan yang harus diambil pada lokasi\n");
+    printf("DROP_OFF -> Mengantarkan item ke lokasi jika item di tumpukan teratas sesuai dengan pesanan\n");
+    printf("MAP -> Memunculkan peta\n");
+    printf("TO_DO -> Menampilkan pesanan yang masuk ke To Do List\n");
+    printf("IN_PROGRESS -> Menampilkan pesanan yang sedang dikerjakan\n");
+    printf("BUY -> Menampilkan gadget yang dapat dibeli lalu membelinya\n");
+    printf("INVENTORY -> Menampilkan list isi inventory\n");
+    printf("HELP -> Menampilkan seluruh command yang dapat dimasukkan beserta deskripsinya\n");
 }
