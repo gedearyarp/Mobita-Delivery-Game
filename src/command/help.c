7358f9852fe0fbfc2a16ca26e8627c4b0a018af4
addinnabilal <16520342@std.stei.itb.ac.id>
2021-11-07 05:57:34 +0700

    add help and edit pickup

---
+++ src/command/help.c
@@ -0,0 +1,13 @@
+void helpCommand() {
+    printf("NEW GAME -> Memulai permainan\n
+    EXIT -> Menutup program\n
+    MOVE -> Berpindah sesuai posisi yang dapat dicapai\n
+    PICK_UP -> Mengambil item jika ada pesanan yang harus diambil pada lokasi\n
+    DROP_OFF -> Mengantarkan item ke lokasi jika item di tumpukan teratas sesuai dengan pesanan\n
+    MAP -> Memunculkan peta\n
+    TO_DO -> Menampilkan pesanan yang masuk ke To Do List\n
+    IN_PROGRESS -> Menampilkan pesanan yang sedang dikerjakan\n
+    BUY -> Menampilkan gadget yang dapat dibeli lalu membelinya\n
+    INVENTORY -> Menampilkan list isi inventory\n
+    HELP -> Menampilkan seluruh command yang dapat dimasukkan beserta deskripsinya\n");
+}
--- src/command/pick_up.c
+++ src/command/pick_up.c
@@ -1,16 +1,21 @@
-#include "../stack/stack.h"
+#include "../tas/tas.h"
 #include "../list_linked/list_linked.h"
 #include "../point/point.h"
-
-void pick_up(Stack *Tas, POINT P, List *to_do_list, List *in_progress_list, int curr_time, float *time_incr, int *cap_incr, int *uncompleted){
+// BIKIN hapus to do list dan pickup utk jenis tertentu
+void pickUpCommand(Stack *Tas, POINT P, List *to_do_list, List *in_progress_list, int curr_time, float *time_incr, int *cap_incr, int *uncompleted){
     Address q;
+    int ctr;
+    Pesanan val;
     q=FIRST(*to_do_list);
+    ctr=0;
     while (!EQ(P, PICK_P(INFO(q))) && q!=NULL){
         q=NEXT(q);
+        ctr++;
     }
     if (EQ(P, PICK_P(INFO(q)))){
         push(Tas, INFO(q));
-        updateInProgress (in_progress_list, *Tas, 0, curr_time, *time_incr, *cap_incr, *uncompleted);
+        updateInProgress (in_progress_list, Tas, 0, curr_time, time_incr, cap_incr, uncompleted);
+        deleteAt(to_do_list, ctr, &val);
         printf("Pesanan berupa ");
         switch(TYPE(TOP(*Tas)) == 'H'){
             case 'N': printf("Normal Item"); break;
