48b3212dc5c28e9e614d2d903cefd89d51ff0169
addinnabilal <16520342@std.stei.itb.ac.id>
2021-11-16 09:13:59 +0700

    driver bug fixes

--- src/COMMAND/driver_pick_up.c
+++ src/COMMAND/driver_pick_up.c
@@ -1,10 +1,10 @@
 #include "pick_up.h"
-
+#include "../ADT/queue/queue.h"
 int main(){
     Tas tas;
     List to_do_list, in_progress_list;
     POINT X, Y;
-    int currTime=0;
+    int currTime=1;
     Pesanan psn1,psn2,psn3,psn4;
     Queue queue_pesanan;
 
@@ -16,24 +16,26 @@ int main(){
     MakePOINT(3,4,'Y');
     MakePOINT(1,2,'X');
 
-    psn1=createPesanan(1, 1, 2, 'A', 5, 6, 'B', 'N', -1, -1);
-    psn2=createPesanan(1, 1, 2, 'A', 7, 8, 'B', 'N', -1, -1);
-    psn3=createPesanan(1, 1, 2, 'A', 9, 10, 'B', 'N', -1, -1);
-    psn4=createPesanan(1, 1, 2, 'A', 11, 12, 'B', 'N', -1, -1);
+    psn1=createPesanan(1, 1, 2, 'X', 5, 6, 'A', 'N', -1, 1);
+    psn2=createPesanan(1, 1, 2, 'X', 7, 8, 'B', 'N', -1, 1);
+    psn3=createPesanan(1, 1, 2, 'X', 9, 10, 'C', 'N', -1, 1);
+    psn4=createPesanan(1, 1, 2, 'X', 11, 12, 'D', 'N', -1, 1);
 
     // masukkan pesanan pada to do list
     enqueue(&queue_pesanan, psn1);
-    insertToDo(&to_do_list, &queue_pesanan, currTime);
     enqueue(&queue_pesanan, psn2);
-    insertToDo(&to_do_list, &queue_pesanan, currTime);
     enqueue(&queue_pesanan, psn3);
-    insertToDo(&to_do_list, &queue_pesanan, currTime);
     enqueue(&queue_pesanan, psn4);
+    displayQueue(queue_pesanan);
+    displayList(to_do_list);
     insertToDo(&to_do_list, &queue_pesanan, currTime);
-
-
+    insertToDo(&to_do_list, &queue_pesanan, currTime);
+    insertToDo(&to_do_list, &queue_pesanan, currTime);
+    insertToDo(&to_do_list, &queue_pesanan, currTime);
+    displayQueue(queue_pesanan);
+    displayList(to_do_list);
     // gagal karena tidak ada item pada list
-    pickUpCommand(&tas, Y, &to_do_list, &in_progress_list, &currTime);
+    pickUpCommand(&tas, X, &to_do_list, &in_progress_list, &currTime);
 
     // berhasil
     pickUpCommand(&tas, X, &to_do_list, &in_progress_list, &currTime);
